5b72cedf72c09d0078626b6e2e92454e99bfa4e9
andersonprovox <andersongfonseca@gmail.com>
2022-09-28 20:25:06 -0300

    criando a home

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  templateUrl:'./products-header.component.html' ,
+ 
+})
+export class ProductsHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
