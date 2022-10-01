fa1dc9d034da8e524f928b461363d4aace6a421a
andersonprovox <andersongfonseca@gmail.com>
2022-10-01 15:08:44 -0300

    finalizando a construção da home page

---
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-box',
+  templateUrl:'product-box.component.html', 
+})
+export class ProductBoxComponent implements OnInit {
+
+  @Input() fullWidthMode = false;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: {[id: number]: number} = { 1: 400, 3: 335, 4: 350};
+
 @Component({
   selector: 'app-home',
   templateUrl: `./home.component.html`
 })
 export class HomeComponent implements OnInit {
 
+  cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
+  category: string | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  oncolumnsCountChange(coulsNum: number): void {
+    this.cols = coulsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
+  }
+
+  onShowCategory(newCategory:string): void {
+    this.category = newCategory;
   }
 
 }
