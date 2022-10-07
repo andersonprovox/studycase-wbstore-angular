61761d0ef5cd0ee1dc0673f8aba3c417ea30b78e
andersonprovox <andersongfonseca@gmail.com>
2022-10-07 10:20:57 -0300

    implementando sistema de pagamentos

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({ extended: false}));
+app.use(bodyparser.json());
+app.use(cors({ origin: true, credentials: true}));
+
+const stripe = require("stripe")("passar-key-seguranca-aqui");
+
+app.post("/checkout", async (req, res, next) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+            line_items: req.body.items.map((item) => ({
+
+                price_data: {
+                    currency: "usd",
+                    product_data: {
+                        name: item.name,
+                        images: [item.product]
+                    },
+                    unit_amount: items.price * 100,
+
+                },
+                quantity: item.quantity,
+            })),
+            mode: "payment",
+            success_url: "http://localhost:4242/success.html",
+            cancel_url: "http://localhost:4242/cancel.html"
+        });
+
+        res.status(200).json(session);
+    } catch (error) {
+        next(error);
+    }
+});
+
+app.listen(4242, () => console.log('app is running on 4242'));
